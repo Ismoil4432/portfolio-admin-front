1769b5da760e82ddfea1649756fb7a86fba176a8
Ismoil4432 <ismoil4432@gmail.com>
2023-07-13 19:24:39 +0500

    commited

---
+++ src/service/axios.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+// axios.defaults.baseURL = "http://localhost:1987/api"
+// axios.defaults.baseURL = "https://backendportfolio-production-07d4.up.railway.app/api"
+axios.defaults.baseURL = "https://portfolio-admin-ls6u.onrender.com/api"
+
+export default axios
